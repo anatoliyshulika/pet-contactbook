6899cf00b7b37e586fdb1df4d7a07d9e9a62ba67
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2023-02-09 17:49:10 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="goit-react-hw-08-phonebook">
+      <BrowserRouter basename="pet-contactbook">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
